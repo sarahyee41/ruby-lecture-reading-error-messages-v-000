537279465c59a1d4443ff42c20ed42a1382557fd
Sarah Yee <sarahyee41@gmail.com>
2018-01-21 01:19:11 +0000

    automatically backed up by learn

---
+++ currentpresident.rb
@@ -0,0 +1,5 @@
+current_president = "Barack Obama"
+puts "In 2016, the president was #{current_president}."
+
+current_president = "Stephen Colbert"
+puts "Now, it being the year 2017, the president is #{current_president}."
